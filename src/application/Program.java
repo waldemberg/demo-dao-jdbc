3b49b74f410e69798f4e9f0d0aa06e4d2992800c
Waldemberg <berg.polimeros@hotmail.com>
2023-06-04 12:01:25 -0300

    insert implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -30,6 +30,11 @@ public class Program {
 		for(Seller obj : list) {
 			System.out.println(obj);
 		}
+		
+		System.out.println("\n=== Teste 4: Seller insert ===");
+		Seller newSeller = new Seller(null, "Greg", "greg@gmail.com", new Date(), 4000.00, department);
+		sellerDao.insert(newSeller);
+		System.out.println("Inserted! New id = " + newSeller.getId());
 
 	}
 	
