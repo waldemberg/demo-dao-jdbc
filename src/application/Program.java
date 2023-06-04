0d24525b00dcbfbe8dc7da3b563c8a85d4db4a16
Waldemberg <berg.polimeros@hotmail.com>
2023-06-04 10:08:18 -0300

    findAll implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -24,6 +24,12 @@ public class Program {
 		for(Seller obj : list) {
 			System.out.println(obj);
 		}
+		
+		System.out.println("\n=== Teste 3: Seller findByAll ===");
+		list = sellerDao.findAll();
+		for(Seller obj : list) {
+			System.out.println(obj);
+		}
 
 	}
 	
